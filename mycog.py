627c696dc4f6cca2a89c57759164e945e9f20bfe
Ryan Patel <supatel@live.com>
2017-03-28 18:34:32 -0700

    New sample
    Changed to hello (similar to punch example) as an exercise

--- mycog.py
+++ mycog.py
@@ -8,11 +8,11 @@ class Mycog:
         self.bot = bot
 
     @commands.command()
-    async def mycom(self):
+    async def greet(self, user : discord.Member):
         """This does stuff!"""
 
-        #Your code will go here
-        await self.bot.say("I can do stuff!")
+        # Your code will go here
+        await self.bot.say("Hello there, " + user.mention + "!")
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
